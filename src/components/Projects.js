3332354fe27480d163d0bfacd712da72b462f670
Kelvin Mue <kelvimue@gmail.com>
2024-02-15 13:39:54 +0300

    Added my first project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import Connecto from "../assets/img/Connecto.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -11,9 +12,10 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Connecto",
+      description: "Empowering Creators And Brands To Navigate The Influencer Marketing Landscape",
+      imgUrl: Connecto,
+      link:"https://connecto-six.vercel.app/",
     },
     {
       title: "Business Startup",
